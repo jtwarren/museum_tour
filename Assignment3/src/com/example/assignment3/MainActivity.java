5486d1d131ba3eb0a049f9cf75bcf3c66afe0557
akashbad <akashbad@mit.edu>
2013-02-20 17:11:59 -0500

    added the button and finished the assignment

--- Assignment3/src/com/example/assignment3/MainActivity.java
+++ Assignment3/src/com/example/assignment3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.assignment3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void changeMessage(View view){
+    	TextView text = (TextView) findViewById(R.id.text_content);
+    	text.setText(R.string.goodbye_world);
     }
     
 }
