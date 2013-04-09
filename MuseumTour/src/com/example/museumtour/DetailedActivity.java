6489a75bb421ba49a306e8d01c9c15aab9491291
Jeffrey Warren <jtwarren@mit.edu>
2013-04-08 22:16:54 -0400

    add detailed activity

--- MuseumTour/src/com/example/museumtour/DetailedActivity.java
+++ MuseumTour/src/com/example/museumtour/DetailedActivity.java
@@ -1,5 +1,11 @@
 package com.example.museumtour;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class DetailedActivity extends Activity {
 
 	@Override
@@ -8,8 +14,28 @@ public class DetailedActivity extends Activity {
 
 	    // Get the message from the intent
 	    Intent intent = getIntent();
+	    int image = intent.getIntExtra("EXHIBIT_IMAGE", -1);
+	    String title = intent.getStringExtra("EXHIBIT_TITLE");
+	    String text = intent.getStringExtra("EXHIBIT_TEXT");
 	    setContentView(R.layout.detailed);
+	    setImage(image);
+	    setTitle(title);
+	    setText(text);
+	    
+	}
+	public void setImage(int image){
+		ImageView imageView = (ImageView)findViewById(R.id.exhibit_image0);
+		imageView.setImageDrawable(getResources().getDrawable(image));
 	}
 	
+	public void setTitle(String title){
+		TextView titleView = (TextView)findViewById(R.id.exhibit_title);
+		titleView.setText(title);
+	}
+	
+	public void setText(String text){
+		TextView textView = (TextView)findViewById(R.id.exhibit_text);
+		textView.setText(text);
+	}
 
 }
